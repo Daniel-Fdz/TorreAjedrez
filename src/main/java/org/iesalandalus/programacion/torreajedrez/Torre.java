9333e736a6dbd12c4c7b1945b2ff6d5b224cbc13
DanielFernandezDiez <pure_code@outlook.com>
2021-11-09 22:08:34 +0100

    Creación de los métodos get y set de la clase Torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -5,5 +5,32 @@ public class Torre {
 	// Atributos de la clase
 	private Color color;
 	private Posicion posicion;
+
+	// Devuelve el color
+	public Color getColor() {
+		return color;
+	}
 	
+	// Establece el color. No puede ser nulo
+	private void setColor(Color color) {
+		if(color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		} else {
+			this.color = color;
+		}
+	}
+	
+	// Devuelve la posición
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	// Establece la posición. No puede ser nula
+	private void setPosicion(Posicion posicion) {
+		if(posicion == null) {
+			throw new NullPointerException("ERROR: La posición no puede ser nula.");
+		} else {
+			this.posicion = posicion;
+		}
+	}
 }
