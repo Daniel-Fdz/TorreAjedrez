fac6dcc823f13ee8959ee0cff7e54eddccf33270
DanielFernandezDiez <pure_code@outlook.com>
2021-11-09 22:31:13 +0100

    Creación del método mover de la clase Torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -64,4 +64,85 @@ public class Torre {
 			this.posicion = posicion;
 		}
 	}
+
+	// Método para el movimiento de la torre. Los pasos deben ser positivos y la dirección no puede ser nula
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+		
+		if(pasos < 1) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe ser positivo.");
+		}
+		
+		if(direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		
+		// Si el color de la torre es negro, invertimos las direcciones
+		if(color.equals(Color.NEGRO)) {
+			Direccion dirDefinitiva = direccion;
+			
+			switch(direccion) {
+				case ARRIBA:
+					dirDefinitiva = Direccion.ABAJO;
+					break;
+				case ABAJO:
+					dirDefinitiva = Direccion.ARRIBA;
+					break;
+				case IZQUIERDA:
+					dirDefinitiva = Direccion.DERECHA;
+					break;
+				case DERECHA:
+					dirDefinitiva = Direccion.IZQUIERDA;
+					break;
+				default:
+					break;
+			}
+			
+			// Guardamos la dirección nueva en "direccion"
+			direccion = dirDefinitiva;
+		}
+		
+		// Comprobamos la dirección y movemos la torre controlando los errores
+		switch(direccion) {
+			case ARRIBA:
+				try {
+					setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna())));
+				} catch(IllegalArgumentException e) {
+					throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+				}
+				
+				break;
+				
+			case ABAJO:
+				try {
+					setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna())));
+				} catch(IllegalArgumentException e) {
+					throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+				}
+				
+				break;
+				
+			case IZQUIERDA:
+				try {
+					setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() - pasos)));
+				} catch(IllegalArgumentException e) {
+					throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+				}
+				
+				break;
+				
+			case DERECHA:
+				try {
+					setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() + pasos)));
+				} catch(IllegalArgumentException e) {
+					throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+				}
+				break;
+				
+			case ENROQUE_LARGO:
+			case ENROQUE_CORTO:
+				enrocar(direccion);
+				break;
+				
+		}
+	}
 }
