5f400e7894da38ebd97eddc0bdd9f21b62d0ed8b
DanielFernandezDiez <pure_code@outlook.com>
2021-11-09 21:40:58 +0100

    Creados métodos get y set de la clase Posicion

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -6,4 +6,31 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	// Devuelve la fila
+	public int getFila() {
+		return fila;
+	}
+	
+	// Establece la fila. Tiene que ser entre 1 y 8
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		} else {
+			this.fila = fila;
+		}
+	}
+	
+	// Devuelve la columna
+	public char getColumna() {
+		return columna;
+	}
+	
+	// Establece la columna. Tiene que ser entre 'a' y 'h'
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		} else {
+			this.columna = columna;
+		}
+	}
 }
